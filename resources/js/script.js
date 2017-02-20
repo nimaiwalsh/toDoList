257e1b34b03b95a18d12f3b15f04a0132c8abbb9
nimaiwalsh <nimai@nimaiwalsh.com>
2017-02-20 20:40:36 +1000

    Version 4

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,26 +1,76 @@
-/*VERSION 3 - Move the functions into a ToDoList object */
-
+/*VERSION 4 - */
+//1. toDoList.addTodo should add objects.
+//2. toDoList.changeTodo should change the toDoText property
+//3. toDoList.toggleCompleted should change the completed property
 var toDoList = {
-    list: ['item 1', 'item 2', 'item 3'],
+    //Store toDos
+    toDos: [],
+    
     displayToDos: function() {
-        console.log(this.list);
+        console.log(this.toDos);
+    }, 
+    
+    //Create objects for toDos.
+    addTodo: function(toDoText) {
+        this.toDos.push({
+            todoText: toDoText,
+            completed: false
+        });
+        this.displayToDos();
     },
     
-    addTodo: function(item) {
-        this.list.push(item);  
+    //Change the toDoText property
+    changeTodo: function(position, todoText) {
+        this.toDos[position].todoText = todoText;
+        this.displayToDos();
     },
     
-    changeTodo: function(position, newItem) {
-        this.list[position] = newItem;
+    //Toggle completed
+    toggleCompleted: function(position) {
+        var todo = this.toDos[position];
+        todo.completed = !todo.completed;
+        this.displayToDos();
     },
     
+    //deleteToDo method
     deleteTodo: function(position) {
-        this.list.splice(position, 1);
+        this.toDos.splice(position, 0 );
         this.displayToDos();
     }
 };
 
-toDoList.deleteTodo(0);
+// ===========================================
+
+///*VERSION 3 - Move the functions into a ToDoList object */
+//
+//var toDoList = {
+//    //Store toDos
+//    list: ['item 1', 'item 2', 'item 3'],
+//    displayToDos: function() {
+//        console.log(this.list);
+//    },
+//    
+//    //addTodo method
+//    addTodo: function(item) {
+//        this.list.push(item);  
+//    },
+//    
+//    //changeTodo method
+//    changeTodo: function(position, newItem) {
+//        this.list[position] = newItem;
+//    },
+//    
+//    //deleteToDo method
+//    deleteTodo: function(position) {
+//        this.list.splice(position, 1);
+//    }
+//};
+//
+//toDoList.addTodo('New item');
+//toDoList.deleteTodo(0);
+//toDoList.displayToDos();
+
+//===============================================
 
 ///*VERSION 2*/
 ////A place to store the ToDos
